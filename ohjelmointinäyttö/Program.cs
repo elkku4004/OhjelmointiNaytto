e41bd821ab7babf9727e43e564eb3e8d2f5c175d
elkku4004 <81323972+elkku4004@users.noreply.github.com>
2023-01-25 10:23:10 +0200

    hgh

--- ohjelmointinäyttö/Program.cs
+++ ohjelmointinäyttö/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security;
 
 namespace ohjelmointinäyttö
 {
@@ -8,21 +9,47 @@ namespace ohjelmointinäyttö
         static void Main(string[] args)
         {
 
-            {
-                Console.Write("Käyttäjätunnus: ");
-                string inputUsername = Console.ReadLine();
 
-                Console.Write("Salasana: ");
-                string inputPassword = Console.ReadLine();
+            Console.Write("Käyttäjätunnus: ");
+            string inputUsername = Console.ReadLine();
 
-            }
-            { }
-             
-    }
+            Console.Write("Salasana: ");
+            SecureString inputPassword = GetPassword();
+
+        }
         // Suorittaa kirjautumisen
         // Jos on oikea tunnus, kirjautuu sisään
         // Jos ei ole niin väärä salasana tai tunnus
+
+
+        static SecureString GetPassword()
+        {
+            var pwd = new SecureString();
+            while (true)
+            {
+                ConsoleKeyInfo i = Console.ReadKey(true);
+                if (i.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                else if (i.Key == ConsoleKey.Backspace)
+                {
+                    if (pwd.Length > 0)
+                    {
+                        pwd.RemoveAt(pwd.Length - 1);
+                        Console.Write("\b \b");
+                    }
+                }
+               
+                else if (i.KeyChar != '\u0000') // KeyChar == '\u0000' if the key pressed does not correspond to a printable character, e.g. F1, Pause-Break, etc
+                {
+                    pwd.AppendChar(i.KeyChar);
+                    Console.Write("*");
+                }
+            }
+            return pwd;
+        }
     }
-        } 
-    
+}
+
 
