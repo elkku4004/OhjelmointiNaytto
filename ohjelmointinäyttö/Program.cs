854e580913f1c8b16973b550456c61a1e2f7f273
elkku4004 <81323972+elkku4004@users.noreply.github.com>
2023-01-25 10:30:38 +0200

    ff

--- ohjelmointinäyttö/Program.cs
+++ ohjelmointinäyttö/Program.cs
@@ -8,7 +8,31 @@ namespace ohjelmointinäyttö
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
 
+                string syöte;
+                Console.WriteLine("Anna komento:");
+                Console.WriteLine("  - rekisteröi");
+                Console.WriteLine("  - kirjaudu");
+                Console.WriteLine("  - poistu");
+                syöte = Console.ReadLine();
+
+                switch (syöte)
+                {
+                    case "rekisteröi":
+
+                        break;
+                    case "kirjaudu":
+
+                        break;
+                    case "poistu":
+                        
+                        break;
+                    default:
+                        break;
+                }
+            }
 
             Console.Write("Käyttäjätunnus: ");
             string inputUsername = Console.ReadLine();
@@ -22,17 +46,27 @@ namespace ohjelmointinäyttö
         // Jos ei ole niin väärä salasana tai tunnus
 
 
+        static void Kirjaudu()
+        {
+
+        }
+
+        static void RekisteröiKäyttäjä()
+        {
+
+        }
+
         static SecureString GetPassword()
         {
             var pwd = new SecureString();
             while (true)
             {
                 ConsoleKeyInfo i = Console.ReadKey(true);
-                if (i.Key == ConsoleKey.Enter)
+                if (i.Key == ConsoleKey.Enter)   // Jos painaa enter, lopettaa kirjoittamisen.
                 {
                     break;
                 }
-                else if (i.Key == ConsoleKey.Backspace)
+                else if (i.Key == ConsoleKey.Backspace)  // Jos painaa backspace niin menee yhden taaksepäin.
                 {
                     if (pwd.Length > 0)
                     {
@@ -40,11 +74,11 @@ namespace ohjelmointinäyttö
                         Console.Write("\b \b");
                     }
                 }
-               
-                else if (i.KeyChar != '\u0000') // KeyChar == '\u0000' if the key pressed does not correspond to a printable character, e.g. F1, Pause-Break, etc
+
+                else if (i.KeyChar != '\u0000')
                 {
                     pwd.AppendChar(i.KeyChar);
-                    Console.Write("*");
+                    Console.Write("*");  // jos painaa jotain näppäintä, se muuttuu *.
                 }
             }
             return pwd;
