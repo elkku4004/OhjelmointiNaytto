d9e6b365d4c77d21e1c960947b1f6352e1a95d8d
elkku4004 <81323972+elkku4004@users.noreply.github.com>
2023-01-25 09:13:04 +0200

    project commit

---
+++ ohjelmointinäyttö/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ohjelmointinäyttö
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Console.Write("Käyttäjätunnus: ");
+                string inputUsername = Console.ReadLine();
+
+                Console.Write("Salasana: ");
+                string inputPassword = Console.ReadLine();
+            }
+            { }
+             
+    }
+        // Suorittaa kirjautumisen
+        // Jos on oikea tunnus, kirjautuu sisään
+        // Jos ei ole niin väärä salasana tai tunnus
+    }
+        } 
+    
+
---
+++ ohjelmointinäyttö/ProgramBase.cs
@@ -0,0 +1,18 @@
+﻿namespace ohjelmointinäyttö
+{
+    internal class ProgramBase
+    {
+        public char PasswordChar { get; set; }
+        static void Main(string[] args)
+        {
+
+            {
+                Console.Write("Käyttäjätunnus: ");
+                string inputUsername = Console.ReadLine();
+
+                Console.Write("Salasana: ");
+                string inputPassword = Console.ReadLine();
+            }
+            { }
+    }
+}
