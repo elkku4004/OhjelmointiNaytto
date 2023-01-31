b2cb56f4cef87052fe9003714861301b2377cf2b
elkku4004 <81323972+elkku4004@users.noreply.github.com>
2023-01-31 11:25:24 +0200

    jou

--- ohjelmointinäyttö/Program.cs
+++ ohjelmointinäyttö/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Security;
+using System.Text;
 
 namespace ohjelmointinäyttö
 {
@@ -9,47 +11,39 @@ namespace ohjelmointinäyttö
     {
 
         static List<Käyttäjä> Käyttäjät = new List<Käyttäjä>();  // käyttäjä luokan lista
+        static SqlConnection conn;
 
         static void Main(string[] args)
         {
-            
-                for (int i = 0; i <= 1; i++)
-                {
+            ConnectDatabase();
+
+
+            for (int i = 0; i <= 1; i++)
+            {
                 string syöte;
                 Console.WriteLine("Anna komento:");
                 Console.WriteLine("  - rekisteröi");
                 Console.WriteLine("  - kirjaudu");
                 Console.WriteLine("  - poistu");
                 syöte = Console.ReadLine();
-                    // Kun avaa konsolin, nämä tulevat näkyviin.
-                    switch (syöte)
-                    {
-                        case "rekisteröi":
-                            RekisteröiKäyttäjä();  // rekisteröi ohjeet
-                            break;
-                        case "kirjaudu":  // kirjaudu ohjeet
-                            Kirjaudu();
-                            break;
-                        case "poistu":  
+                // Kun avaa konsolin, nämä tulevat näkyviin.
+                switch (syöte)
+                {
+                    case "rekisteröi":
+                        RekisteröiKäyttäjä();  // rekisteröi ohjeet
+                        break;
+                    case "kirjaudu":  // kirjaudu ohjeet
+                        Kirjaudu();
+                        break;
+                    case "poistu":
                         Poistu();   // poistu ohjeet
 
-                            break;
-                        default:
-                                break;
-                        // loopin endaus, vittu miten tää toimii perkele
-                        {
-                            break;
-                        }
+                        break;
+                    default:
+                        break;
 
-                    }
+                }
             }
-
-            Console.Write("Käyttäjätunnus: ");
-            string inputUsername = Console.ReadLine();
-
-            Console.Write("Salasana: ");
-            SecureString inputPassword = GetPassword();
-
         }
         // Suorittaa kirjautumisen
         // Jos on oikea tunnus, kirjautuu sisään
@@ -77,15 +71,60 @@ namespace ohjelmointinäyttö
 
             Käyttäjä uusiKäyttäjä = new Käyttäjä();
             uusiKäyttäjä.KäyttäjäTunnus = inputUsername;  // uuden käyttäjän luominen
+            uusiKäyttäjä.Salasana = inputPassword;  // uuden käyttäjän luominen
 
+            //create a new SQL Query using StringBuilder
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.Append("INSERT INTO Users VALUES ");
+            strBuilder.Append("('" + uusiKäyttäjä.KäyttäjäTunnus + "','" + uusiKäyttäjä.Salasana + "','" + DateTime.Now.ToString() +"');");
+
+            string sqlQuery = strBuilder.ToString();
+
+            using (SqlCommand command = new SqlCommand(sqlQuery, conn)) //pass SQL query created above and connection
+            {
+                command.ExecuteNonQuery(); //execute the Query
+                Console.WriteLine("");
+                Console.WriteLine("Query Executed.");
+            }
         }
 
         static void Poistu()
         {
             Console.Write("Poistu");
-           
-                // tähänki pitäs heittää ny jotai
+
+            // tähänki pitäs heittää ny jotai
         }
+
+        static void ConnectDatabase()
+        {
+            Console.WriteLine("Getting Connection ...");
+
+            var datasource = @"STUDY21P-104827\SQLEXPRESS";//your server
+            var database = "Ohjelmointi"; //your database name
+
+            //your connection string 
+            string connString = @"Server=" + datasource + ";Database="
+                        + database + ";Trusted_Connection=SSPI;Encrypt=False;TrustServerCertificate=true";
+
+            //create instanace of database connection
+            conn = new SqlConnection(connString);
+
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                //open connection
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
 
         static SecureString GetPassword()  // salasanan maskeeramis metodi securestring
         {
@@ -115,7 +154,7 @@ namespace ohjelmointinäyttö
             return pwd;
         }
     }
-} 
+}
 
 
 // todo lista
